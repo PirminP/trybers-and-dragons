171310657c785713b25be2e9fbc4fbfcca56bf00
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-27 13:52:41 -0300

    feat: Creating subclasses of Race

---
+++ src/Races/Elf.ts
@@ -0,0 +1,30 @@
+import Race from './Race';
+
+class Elf extends Race {
+  private _maxLifePoints: number;
+  private static _createdInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+    Elf._createdInstances += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get dexterity(): number {
+    return this.dexterity;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdInstances;
+  }
+}
+
+export default Elf;
---
+++ src/Races/Orc.ts
@@ -0,0 +1,30 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _createdInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._createdInstances += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get dexterity(): number {
+    return this.dexterity;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return this._createdInstances;
+  }
+}
+
+export default Orc;
