213bf00ed2987710b48c826f8db61bb917db9a4c
PirminP <pirmin.pernsteiner@gmail.com>
2022-09-27 22:06:22 -0300

    feat: Creating subclasses of Archetype

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,35 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _createdInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._createdInstances += 1;
+  }
+
+  get name(): string {
+    return this.name;
+  }
+
+  get special(): number {
+    return this.special;
+  }
+
+  get cost(): number {
+    return this.cost;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._createdInstances;
+  }
+}
+
+export default Warrior;
